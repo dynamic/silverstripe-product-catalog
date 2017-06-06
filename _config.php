960bb735859a8191ee86d66bf86b4e4dc1906885
Jason Irish <jirish@dynamicagency.com>
2017-06-06 14:04:33 -0500

    Show products in XML sitemap

--- _config.php
+++ _config.php
@@ -2,3 +2,7 @@
 
 define('SILVERSTRIPE_PRODUCT-CATALOG_PATH', __DIR__);
 define('SILVERSTRIPE_PRODUCT-CATALOG_DIR', basename(__DIR__));
+
+if (method_exists('GoogleSitemap', 'register_dataobject')) {
+    GoogleSitemap::register_dataobject('CatalogProduct', 'daily', 1);
+}
